f4cbe0cfcf8d44b02da1c96b24ad68178cdffc88
intuinno <intuinno@gmail.com>
2020-03-04 17:28:58 -0600

    Added baby agent, added controller for python interface

---
+++ Assets/scripts/BabyAgent.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents.Sensor;
+using MLAgents;
+using DAIVID;
+
+public class BabyAgent : Agent
+{
+    public Transform hips;
+    public Transform chest;
+    public Transform spine;
+    public Transform head;
+    public Transform thighL;
+    public Transform shinL;
+    public Transform footL;
+    public Transform thighR;
+    public Transform shinR;
+    public Transform footR;
+    public Transform upperArmL;
+    public Transform lowerArmL;
+    public Transform handL;
+    public Transform upperArmR;
+    public Transform lowerArmR;
+    public Transform handR;
+    JointDriveController m_JdController;
+
+    Rigidbody m_HipsRb;
+    Rigidbody m_ChestRb;
+    Rigidbody m_SpineRb;
+
+    IFloatProperties m_ResetParams;
+    public override void InitializeAgent()
+    {
+        m_JdController = GetComponent<JointDriveController>();
+        m_JdController.SetupBodyPart(hips);
+        m_JdController.SetupBodyPart(chest);
+        m_JdController.SetupBodyPart(spine);
+        m_JdController.SetupBodyPart(head);
+        m_JdController.SetupBodyPart(thighL);
+        m_JdController.SetupBodyPart(shinL);
+        m_JdController.SetupBodyPart(footL);
+        m_JdController.SetupBodyPart(thighR);
+        m_JdController.SetupBodyPart(shinR);
+        m_JdController.SetupBodyPart(footR);
+        m_JdController.SetupBodyPart(upperArmL);
+        m_JdController.SetupBodyPart(lowerArmL);
+        m_JdController.SetupBodyPart(handL);
+        m_JdController.SetupBodyPart(upperArmR);
+        m_JdController.SetupBodyPart(lowerArmR);
+        m_JdController.SetupBodyPart(handR);
+
+        m_HipsRb = hips.GetComponent<Rigidbody>();
+        m_ChestRb = chest.GetComponent<Rigidbody>();
+        m_SpineRb = spine.GetComponent<Rigidbody>();
+
+        m_ResetParams = Academy.Instance.FloatProperties;
+
+        SetResetParameters();
+
+    }
+
+    public void SetTorsoMass()
+    {
+        m_ChestRb.mass = m_ResetParams.GetPropertyWithDefault("chest_mass", 5);
+        m_SpineRb.mass = m_ResetParams.GetPropertyWithDefault("spine_mass", 7);
+        m_HipsRb.mass = m_ResetParams.GetPropertyWithDefault("hip_mass", 6);
+    }
+
+    public void SetResetParameters()
+    {
+        SetTorsoMass();
+    }
+
+    /// <summary>
+    /// Loop over body parts to add them to observation.
+    /// </summary>
+    public override void CollectObservations()
+    {
+
+        //babyHeadCam.
+        m_JdController.GetCurrentJointForces();
+
+        //sensor.AddObservation(m_DirToTarget.normalized);
+        AddVectorObs(m_JdController.bodyPartsDict[hips].rb.position);
+        AddVectorObs(hips.forward);
+        AddVectorObs(hips.up);
+
+        foreach (var bodyPart in m_JdController.bodyPartsDict.Values)
+        {
+            CollectObservationBodyPart(bodyPart);
+        }
+    }
+
+    /// <summary>
+    /// Add relevant information on each body part to observations.
+    /// </summary>
+    public void CollectObservationBodyPart(BodyPart bp)
+    {
+        var rb = bp.rb;
+        AddVectorObs(rb.velocity);
+        AddVectorObs(rb.angularVelocity);
+        var localPosRelToHips = hips.InverseTransformPoint(rb.position);
+        AddVectorObs(localPosRelToHips);
+
+
+        if (bp.rb.transform != hips && bp.rb.transform != head)
+        {
+            AddVectorObs(bp.currentXNormalizedRot);
+            AddVectorObs(bp.currentYNormalizedRot);
+            AddVectorObs(bp.currentZNormalizedRot);
+            AddVectorObs(bp.currentStrength / m_JdController.maxJointForceLimit);
+        }
+    }
+
+    /// <summary>
+    /// Loop over body parts and reset them to initial conditions.
+    /// </summary>
+    public override void AgentReset()
+    {
+        //if (m_DirToTarget != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.LookRotation(m_DirToTarget);
+        //}
+
+        foreach (var bodyPart in m_JdController.bodyPartsDict.Values)
+        {
+            bodyPart.Reset(bodyPart);
+        }
+        SetResetParameters();
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        //Debug.Break();
+        var bpDict = m_JdController.bodyPartsDict;
+        var i = -1;
+
+        bpDict[chest].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], vectorAction[++i]);
+        bpDict[spine].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], vectorAction[++i]);
+
+        bpDict[thighL].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[thighR].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[shinL].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[shinR].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[footR].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], vectorAction[++i]);
+        bpDict[footL].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], vectorAction[++i]);
+
+
+        bpDict[upperArmL].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[upperArmR].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[lowerArmL].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[lowerArmR].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[head].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+
+        //update joint strength settings
+        bpDict[chest].SetJointStrength(vectorAction[++i]);
+        bpDict[spine].SetJointStrength(vectorAction[++i]);
+        bpDict[head].SetJointStrength(vectorAction[++i]);
+        bpDict[thighL].SetJointStrength(vectorAction[++i]);
+        bpDict[shinL].SetJointStrength(vectorAction[++i]);
+        bpDict[footL].SetJointStrength(vectorAction[++i]);
+        bpDict[thighR].SetJointStrength(vectorAction[++i]);
+        bpDict[shinR].SetJointStrength(vectorAction[++i]);
+        bpDict[footR].SetJointStrength(vectorAction[++i]);
+        bpDict[upperArmL].SetJointStrength(vectorAction[++i]);
+        bpDict[lowerArmL].SetJointStrength(vectorAction[++i]);
+        bpDict[upperArmR].SetJointStrength(vectorAction[++i]);
+        bpDict[lowerArmR].SetJointStrength(vectorAction[++i]);
+    }
+
+    public override float[] Heuristic()
+    {
+        var action = new float[31];
+        Debug.Log("Called hurestic");
+
+        for (int i = 0; i < 31; i++)
+        {
+            action[i] = Random.Range(-1.0f, 1.0f);
+        }
+
+        return action;
+    }
+}
+
