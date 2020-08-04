a58bbaa31fb3f178cf0d7b0ba4ff74bd192f5f30
Ashaduzzaman Rubel <ashaduzzamanrubel@gmail.com>
2020-08-04 11:16:49 -0500

    Fixed animation issue.
    Carla was still moving after reaching the destination. Also at the beginning of walking it was sliding with idle animation.

--- Assets/scripts/Mother.cs
+++ Assets/scripts/Mother.cs
@@ -30,30 +30,15 @@ public class Mother : MonoBehaviour
         {
             Debug.LogWarning("Mother doesn't have any Animator attached");
         }
-
-        //Invo
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 direction = baby.position - transform.position;
-        //direction = direction.normalized * modelAnimator.runtimeAnimatorController.animationClips[0].averageSpeed.magnitude;
-        //momController.Move(direction);
-
-        if(Vector3.Distance(baby.position, transform.position) >= .9)
+        if(Vector3.Distance(baby.position, transform.position) >= agent.stoppingDistance)
         {
             agent.SetDestination(baby.position);
-            Debug.Log("Moving");
             modelAnimator.SetBool("move", true);
-            //modelAnimator.
-            //Vector3 _direction = (baby.position - transform.position).normalized;
-
-            ////create the rotation we need to be in to look at the target
-            //Quaternion _lookRotation = Quaternion.LookRotation(_direction);
-
-            ////rotate us over time according to speed until we are in the required rotation
-            //transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
         }
         else
         {
