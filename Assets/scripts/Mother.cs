1b05500c0d6643755cce65302f066a15d3e020de
Ashaduzzaman Rubel <ashaduzzamanrubel@gmail.com>
2020-08-04 10:24:18 -0500

    Added NavMesh On mother so that it can move towards baby's current position.

---
+++ Assets/scripts/Mother.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mother : MonoBehaviour
+{
+
+    private CharacterController momController;
+    private Animator modelAnimator;
+
+    private float rotationSpeed = 1f;
+
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform baby;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        momController = GetComponent<CharacterController>();
+        agent = GetComponent<NavMeshAgent>();
+        if (!momController)
+        {
+            Debug.LogError("Mother doesn't have any Character Controller attached");
+        }
+        modelAnimator = GetComponent<Animator>();
+        if (!modelAnimator)
+        {
+            Debug.LogWarning("Mother doesn't have any Animator attached");
+        }
+
+        //Invo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 direction = baby.position - transform.position;
+        //direction = direction.normalized * modelAnimator.runtimeAnimatorController.animationClips[0].averageSpeed.magnitude;
+        //momController.Move(direction);
+
+        if(Vector3.Distance(baby.position, transform.position) >= .9)
+        {
+            agent.SetDestination(baby.position);
+            Debug.Log("Moving");
+            modelAnimator.SetBool("move", true);
+            //modelAnimator.
+            //Vector3 _direction = (baby.position - transform.position).normalized;
+
+            ////create the rotation we need to be in to look at the target
+            //Quaternion _lookRotation = Quaternion.LookRotation(_direction);
+
+            ////rotate us over time according to speed until we are in the required rotation
+            //transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            modelAnimator.SetBool("move", false);
+        }
+
+        
+    }
+}
---
+++ Assets/scripts/MotherController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MotherController:MonoBehaviour
+{
+    private IEnumerator feedRoutine;
+
+    static Lazy<MotherController> s_Lazy = new Lazy<MotherController>(() => new MotherController());
+
+
+    public static bool IsInitialized
+    {
+        get { return s_Lazy.IsValueCreated; }
+    }
+
+    public static MotherController Instance { get { return s_Lazy.Value; } }
+
+    private MotherController()
+    {
+        StartCoroutine(FeedBaby(1));
+    }
+
+    IEnumerator FeedBaby(float time)
+    {
+        yield return null;
+    }
+}
