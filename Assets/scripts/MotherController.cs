2778778ecc50749541937d226595f2a6ac1e1e04
Ashaduzzaman Rubel <ashaduzzamanrubel@gmail.com>
2020-08-17 11:12:35 -0500

    Minor Refactoring

--- Assets/scripts/MotherController.cs
+++ Assets/scripts/MotherController.cs
@@ -47,10 +47,10 @@ public class MotherController:MonoBehaviour
     {
 
         float currentTime = EnvironmentController.Instance.GetCurrentHour();
-        
+
         if (currentTime == 8 || currentTime == 12 || currentTime == 18)
         {
-            Debug.Log("currentTime: " + currentTime);
+            //Debug.Log("currentTime: " + currentTime);
             mother.Feed(FeedingCompleted);
         }
 
