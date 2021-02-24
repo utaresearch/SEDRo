4d330a94cba78d6c6a1f78319de920fca5585393
Aishwarya Pothula <aishwarya.pothula@mavs.uta.edu>
2021-02-24 03:43:39 -0600

    Added 5 experiments for Unity Perception

---
+++ Assets/scripts/Scripts_UP/RodMovement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RodMovement : MonoBehaviour
+{
+    // start and end markers for the path.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // movement speed in units per second.
+    public float speed = 0.025F;
+
+    // time at start of movement
+    private float startTime;
+
+    // distance between the markers.
+    private float pathLength;
+
+    // temporary variables to interchange positions of markers to set reverse motion
+    Vector3 tempPos1, tempPos2;
+    
+
+    void Start()
+    {
+        // to be used as offset when you want to reset timer
+        startTime = Time.time;
+
+
+        // to calculate path length between markers
+        pathLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+   
+
+    // To and fro movement between the markers
+    void Update()
+    {
+
+        // distance moved equals elapsed time elapsed * speed. StartTime = 0 till it is reset
+        float distCovered = (Time.time - startTime) * speed;
+
+        // fraction of path covered in the time elapsed 
+        float fractionOfPath = distCovered / pathLength;
+
+        // set our position as a fraction of the distance between the markers. movement from start to end once
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfPath);
+
+        // once gameobject reaches the end marker
+        if (transform.position == endMarker.position)
+        {
+            // setting the variables for return from end to start
+            startTime = Time.time;
+            tempPos1 = startMarker.position;
+            startMarker.position = endMarker.position;
+            endMarker.position = tempPos1;
+        }
+
+        // once the gameobject reaches the start again
+        if (transform.position == tempPos1)
+        {
+            //setting variables for journey from start to end again
+            startTime = Time.time;
+            tempPos2 = startMarker.position;
+            startMarker.position = tempPos1;
+            endMarker.position = tempPos2;
+        }
+
+
+    }
+
+}
---
+++ Assets/scripts/Scripts_UP/SquareMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareMovement : MonoBehaviour
+{
+    // distance covered after each frame
+    float distanceCovered;
+
+    // diameter of revolution of the object
+    float circleDiameter;
+
+    // speed in deg/sec
+    float speed;
+
+    float z;
+
+    public Transform block;
+
+
+    void Start()
+    {
+        //initializing variables
+
+        distanceCovered = 0;
+        circleDiameter = (block.transform.position.x + 0.025F) - (block.transform.position.x - 0.025F);
+        // converting speed to rad/sec as mathf functions accept only rad/sec.
+        speed = 45F * Mathf.Deg2Rad;
+        z = transform.localPosition.z;
+    }
+
+
+    void Update()
+    {
+        // multiplying s * t to get d
+        distanceCovered += Time.deltaTime * speed;
+
+        // stimulus movement control
+
+        float x = Mathf.Cos(distanceCovered) * circleDiameter;
+        float y = Mathf.Sin(distanceCovered) * circleDiameter;
+        transform.position = new Vector3(x, y, z);
+    }
+}
